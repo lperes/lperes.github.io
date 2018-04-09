817bac8f8c2c104fab1b1b42a9f31945b939226d
Laura <lau.peres@gmail.com>
2018-04-09 17:57:31 -0400

    new

--- testSketch.js
+++ testSketch.js
@@ -1,15 +1,28 @@
+var fs = false;
+
 function setup() {
 
-size(400,400);
+createCanvas(windowWidth, windowHeight);
+colorMode(HSB);
+print("hi");
 
 }
 
 function draw() {
 
   background(0);
-  pushMatrix();
-  translate(width / 2, height /2 );
-  ellipse(0, 0, random(40,50), random(40,50))
-  popMatrix();
 
+  push();
+  noStroke();
+  fill(200,200,255,100);
+  translate(width / 2, height /2 );
+  ellipse(0, 0, random(40,50), random(40,50));
+  fill(32,200,255,100);
+  ellipse(0, 0, random(40,50), random(40,50));
+  pop();
+
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
 }
