b42e653c3f361a9ac91bf6b4d6dc100f587e2093
Laura <lau.peres@gmail.com>
2018-04-15 18:18:05 -0400

    new

--- testSketch.js
+++ testSketch.js
@@ -1,28 +1,66 @@
 var fs = false;
+var circles = [];
+var hue = 0;
+
 
 function setup() {
 
 createCanvas(windowWidth, windowHeight);
-colorMode(HSB);
-print("hi");
+colorMode(HSB, 255);
+
 
 }
 
 function draw() {
+  background(255);
 
-  background(0);
+  //add descpriptive text
+  textSize(32);
+  textAlign(CENTER);
+  text('click and drag', windowWidth/2, windowHeight/2);
+  fill(0, 102, 153);
 
-  push();
-  noStroke();
-  fill(200,200,255,100);
-  translate(width / 2, height /2 );
-  ellipse(0, 0, random(40,50), random(40,50));
-  fill(32,200,255,100);
-  ellipse(0, 0, random(40,50), random(40,50));
-  pop();
-
+//call fuctions
+for (var i= 0; i<circles.length; i++) {
+  circles[i].move();
+  circles[i].display();
+}
 }
 
+//make window resize
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
+}
+
+//if mouse is dragged, new circles are added to canvas in the mouse position
+function mouseDragged(){
+circles.push(new Circle(mouseX, mouseY));
+}
+
+
+//setup the constructor function
+//assign x and y values to setup the drag click functionality
+function Circle(x, y){
+  this.x=x;
+  this.y=y;
+
+//display function
+  this.display = function(){
+    push();
+    noStroke();
+    fill(hue,200,200, 50);
+    ellipse(this.x, this.y, 100, 100);
+    hue=(random(255));
+    pop();
+  }
+
+//move function
+  this.move = function(){
+
+    this.y = this.y + random(1,20);
+    if(this.y>= height){
+      this.y=height;
+    }
+
+  }
 }
